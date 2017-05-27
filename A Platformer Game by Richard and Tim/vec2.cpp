eec2c241b62c3d5ab4e095c67e075bdc0acbd422
timstr <f2a0b@ugrad.cs.ubc.ca>
2017-05-27 12:30:40 -0700

    Added matrix math for vectors

--- A Platformer Game by Richard and Tim/vec2.cpp
+++ A Platformer Game by Richard and Tim/vec2.cpp
@@ -1,6 +1,28 @@
 #pragma once
 #include "vec2.h"
 
+mat2x2 rotationMatrix(double radians){
+	double cosine = cos(radians);
+	double sine = sin(radians);
+	return mat2x2(cosine, -sine, sine, cosine);
+}
+
+mat2x2 operator*(const mat2x2& l, const mat2x2& r){
+	return mat2x2(
+		l.a * r.a + l.b * r.c,
+		l.a * r.b + l.b * r.d,
+		l.c * r.a + l.d * r.c,
+		l.c * r.b + l.d * r.d
+	);
+}
+
+vec2 operator*(const mat2x2& m, const vec2& v){
+	return vec2(
+		m.a * v.x + m.b * v.y,
+		m.c * v.x + m.d * v.y
+	);
+}
+
 double dot(const vec2& v1, const vec2& v2){
 	return (v1.x * v2.x + v1.y * v2.y);
 }
--- A Platformer Game by Richard and Tim/vec2.h
+++ A Platformer Game by Richard and Tim/vec2.h
@@ -4,6 +4,29 @@
 
 typedef sf::Vector2f vec2;
 
+struct mat2x2 {
+	mat2x2(float topleft = 0, float topright = 0, float bottomleft = 0, float bottomright = 0){
+		a = topleft;
+		b = topright;
+		c = bottomleft;
+		d = bottomright;
+	}
+	mat2x2(const vec2& l, const vec2& r){
+		a = l.x;
+		b = r.x;
+		c = l.y;
+		d = r.y;
+	}
+
+	float a, b, c, d;
+};
+
+mat2x2 rotationMatrix(double radians);
+
+mat2x2 operator*(const mat2x2& l, const mat2x2& r);
+
+vec2 operator*(const mat2x2& m, const vec2& v);
+
 const double pi = 3.14159265358979323846264338327950288;
 
 double dot(const vec2& v1, const vec2& v2);
