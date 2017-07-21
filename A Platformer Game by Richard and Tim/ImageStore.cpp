4d10324e0295413561f0082e71c8417f845b2e9e
timstr <f2a0b@ugrad.cs.ubc.ca>
2017-07-21 12:13:29 -0700

    ImageStore

---
+++ A Platformer Game by Richard and Tim/ImageStore.cpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "ImageStore.h"
+
+const sf::Image& ImageStore::getImage(std::string filename){
+	auto it = image_map.find(filename);
+	if (it == image_map.end()){
+		if (!image_map[filename].loadFromFile("images/" + filename)){
+			throw std::runtime_error("The image could not be loaded");
+		}
+		return image_map[filename];
+	} else {
+		return it->second;
+	}
+}
+
+const sf::Texture& ImageStore::getTexture(std::string filename, bool load_image_too){
+	auto it = texture_map.find(filename);
+	if (it == texture_map.end()){
+		auto it2 = image_map.find(filename);
+		if (it2 != image_map.end()){
+			texture_map[filename].loadFromImage(it2->second);
+		} else {
+			if (load_image_too){
+				texture_map[filename].loadFromImage(getImage(filename));
+			} else {
+				if (!texture_map[filename].loadFromFile("images/" + filename)){
+					throw std::runtime_error("The texture could not be loaded");
+				}
+			}
+		}
+
+		return texture_map[filename];
+	} else {
+		return it->second;
+	}
+}
+
+void ImageStore::discardImage(std::string filename){
+	auto it = image_map.find(filename);
+	if (it != image_map.end()){
+		image_map.erase(it);
+	}
+}
+
+void ImageStore::discardTexture(std::string filename){
+	auto it = texture_map.find(filename);
+	if (it != texture_map.end()){
+		texture_map.erase(it);
+	}
+}
+
+void ImageStore::clear(){
+	image_map.clear();
+	texture_map.clear();
+}
+
+ImageStore::ImageStore(){
+
+}
+ImageStore::~ImageStore(){
+	clear();
+}
+
+std::map<std::string, sf::Image> ImageStore::image_map;
+std::map<std::string, sf::Texture> ImageStore::texture_map;
--- A Platformer Game by Richard and Tim/ImageStore.h
+++ A Platformer Game by Richard and Tim/ImageStore.h
@@ -3,82 +3,35 @@
 #include <map>
 #include <SFML\Graphics.hpp>
 
+// TODO: count references to images and textures to automatically manage lifetimes?
+
 struct ImageStore {
 	// returns an Image object from the image file found at the given path
 	// or throws an exception if it can't be loaded
-	static const sf::Image& getImage(std::string filename){
-		auto it = image_map.find(filename);
-		if (it == image_map.end()){
-			if (!image_map[filename].loadFromFile("images/" + filename)){
-				throw std::runtime_error("The image could not be loaded");
-			}
-			return image_map[filename];
-		} else {
-			return it->second;
-		}
-	}
+	static const sf::Image& getImage(std::string filename);
 
 	// returns a Texture object from the image file found at the given path,
 	// if the file is already loaded as an image, it will be reused
 	// throws an exception if it can't be loaded
-	static const sf::Texture& getTexture(std::string filename, bool load_image_too = true){
-		auto it = texture_map.find(filename);
-		if (it == texture_map.end()){
-			auto it2 = image_map.find(filename);
-			if (it2 != image_map.end()){
-				texture_map[filename].loadFromImage(it2->second);
-			} else {
-				if (load_image_too){
-					texture_map[filename].loadFromImage(getImage(filename));
-				} else {
-					if (!texture_map[filename].loadFromFile("images/" + filename)){
-						throw std::runtime_error("The texture could not be loaded");
-					}
-				}
-			}
-
-			return texture_map[filename];
-		} else {
-			return it->second;
-		}
-	}
+	static const sf::Texture& getTexture(std::string filename, bool load_image_too = true);
 
 	// frees the memory used by the image object
 	// obvious potential for dangling references, so use wisely
-	static void discardImage(std::string filename){
-		auto it = image_map.find(filename);
-		if (it != image_map.end()){
-			image_map.erase(it);
-		}
-	}
+	static void discardImage(std::string filename);
 
 	// frees the memory used by the texture object
 	// obvious potential for dangling references, so use wisely
-	static void discardTexture(std::string filename){
-		auto it = texture_map.find(filename);
-		if (it != texture_map.end()){
-			texture_map.erase(it);
-		}
-	}
+	static void discardTexture(std::string filename);
 
 	// frees all memory used by the image and texture objects
 	// use only when safe
-	static void clear(){
-		image_map.clear();
-		texture_map.clear();
-	}
+	static void clear();
 
 	private:
 
-	ImageStore(){
-
-	}
-	~ImageStore(){
-		clear();
-	}
+	ImageStore();
+	~ImageStore();
 
 	static std::map<std::string, sf::Image> image_map;
 	static std::map<std::string, sf::Texture> texture_map;
 };
-std::map<std::string, sf::Image> ImageStore::image_map;
-std::map<std::string, sf::Texture> ImageStore::texture_map;
