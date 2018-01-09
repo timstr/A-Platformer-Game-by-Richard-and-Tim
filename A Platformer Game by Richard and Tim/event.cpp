c743c0833e270b5974c47b77a3254a5ab17f235a
timstr <f2a0b@ugrad.cs.ubc.ca>
2018-01-08 22:37:14 -0800

    Renamed CreatureEvent back to Event for generality

---
+++ A Platformer Game by Richard and Tim/event.cpp
@@ -0,0 +1,26 @@
+#include "event.h"
+
+Event::Event() : id(getNextId()) {
+
+}
+
+int Event::next_id = 0;
+
+int Event::getNextId(){
+	next_id += 1;
+	return next_id;
+}
+
+bool operator<(const Event& l, const Event& r){
+	return l.id < r.id;
+}
+bool operator==(const Event& l, const Event& r){
+	return l.id == r.id;
+}
+
+//////////////////////////
+
+Event Tick; // every frame
+Event Flying; // when airborne
+Event Standing; // when grounded
+Event Impact; // upon collision
